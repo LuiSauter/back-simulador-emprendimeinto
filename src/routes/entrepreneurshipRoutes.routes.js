611eb35b4e18f7025174fa17e4ed50094fe5f5ff
Luis Gabriel Janco <janco7249@gmail.com>
2023-11-27 02:49:47 -0400

    feat: add routes

---
+++ src/routes/entrepreneurshipRoutes.routes.js
@@ -0,0 +1,11 @@
+import { Router } from 'express'
+// import { register, login, verifyToken, logout } from '../controllers/.controller.js'
+
+const router = Router()
+
+// router.post('/register', register)
+// router.post('/login', login)
+// router.get('/verify', verifyToken)
+// router.post('/logout', verifyToken, logout)
+
+export default router
